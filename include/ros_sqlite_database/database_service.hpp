40b22fdfc07f8baeea2b91a4721edf6ef1f89fd0
hackin zhao <hackin.zhao@stratosphere.mobi>
2022-03-21 15:56:50 +0800

    add where condition for query option
    Signed-off-by: hackin zhao <hackin.zhao@stratosphere.mobi>

--- include/ros_sqlite_database/database_service.hpp
+++ include/ros_sqlite_database/database_service.hpp
@@ -3,6 +3,7 @@
 
 #include <ros/ros.h>
 #include "ros_sqlite_database/message_option.hpp"
+#include "twc_database/utility.hpp"
 
 namespace database
 {
@@ -13,6 +14,7 @@ namespace database
         using ServiceType = ServiceT;
         using RequestType = typename ServiceT::Request;
         using ResponseType = typename ServiceT::Response;
+        using Msg = decltype(std::declval<RequestType>().data);
 
         DatabaseService(ros::NodeHandle &nh, const std::string &srv_name) : srv_name_(srv_name)
         {
@@ -25,6 +27,44 @@ namespace database
 
         ~DatabaseService() { server_.shutdown(); };
 
+        template <typename T>
+        bool setParam(T &&value, int i, std::string &string_value)
+        {
+            using U = std::remove_const_t<std::remove_reference_t<T>>;
+            if constexpr (std::is_integral_v<U>)
+            {
+                if (value != 0)
+                    string_value = std::to_string(value);
+                else
+                    return false;
+            }
+            else if constexpr (std::is_floating_point_v<U>)
+            {
+                if (value != 0.0)
+                    string_value = std::to_string(value);
+                else
+                    return false;
+            }
+            else if constexpr (std::is_same_v<std::string, U>)
+            {
+                if (!value.empty())
+                    string_value = "'" + value + "'";
+                else
+                    return false;
+            }
+            else if constexpr (sqlite_wapper::is_char_array_v<U>)
+            {
+                if (sizeof(U))
+                    string_value = "'" + std::string(value, sizeof(U)) + "'";
+                else
+                    return false;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
     protected:
         bool DBcallback(RequestType &req, ResponseType &res)
         {
@@ -32,7 +72,30 @@ namespace database
 
             if (req.option == RequestType::SELECT)
             {
-                res.data = db_opt_.query();
+                Msg msg = req.data;
+                std::string sql;
+
+                int index = 0;
+                iguana::for_each(msg, [&sql, &msg, &index, this](auto item, auto i)
+                                 {
+                    std::string key = iguana::get_name<Msg>(decltype(i)::value).data();
+                    std::string value;
+                    if (setParam(msg.*item, index+1, value))
+                    {
+                        sql += key + "=" + value + " and ";
+                    }
+                    index++; });
+
+                if (sql.empty())
+                {
+                    res.data = db_opt_.query();
+                }
+                else
+                {
+                    sql = sql.substr(0, sql.length() - strlen("and "));
+                    sql = " where " + sql;
+                    res.data = db_opt_.query(sql);
+                }
             }
             else if (req.option == RequestType::INSERT)
             {
@@ -50,12 +113,14 @@ namespace database
             {
                 ROS_WARN_STREAM("database not support option: " << req.option);
             }
+
+            return true;
         }
 
     private:
         const std::string srv_name_;
         ros::ServiceServer server_;
-        ros_sqlite_database::DatabaseOption<decltype(std::declval<RequestType>().data)> db_opt_;
+        ros_sqlite_database::DatabaseOption<Msg> db_opt_;
     };
 
 } // namespace database
